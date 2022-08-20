4b0f34e21796aa967b3752fae27557220b991f52
Marco de Carvalho <marcodecarvalho04@gmail.com>
2022-08-19 22:27:54 -0300

    Mudança na 3-14

--- lista 03/MAPC-3-14.c
+++ lista 03/MAPC-3-14.c
@@ -4,6 +4,22 @@
 
 int main(){
     setlocale(LC_ALL, "");
-    
+    char nome[20][5], sexo[1][5];
+    double salario[5];
+    for (int i = 0; i < 5; i++)
+    {
+        printf("\nInforme o nome: ");
+        gets(nome[i]);
+        printf("Informe o sexo, M ou F: ");
+        gets(sexo[i]);
+        printf("Informe o salário: ");
+        scanf("%lf%*c", &salario[i]);
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        //if (){
+//
+        //}
+    }
     return 0;
 }
